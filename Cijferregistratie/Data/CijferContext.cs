d242b0e8685b031c0191e298dbef7d94d4afae26
itzantoine <49227434+itzantoine@users.noreply.github.com>
2023-03-29 01:16:18 +0200

    first commit

---
+++ Cijferregistratie/Data/CijferContext.cs
@@ -0,0 +1,48 @@
+﻿using Cijferregistratie.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cijferregistratie.Data;
+
+public class CijferContext : DbContext
+{
+    public DbSet<Vak> DbVak { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        
+        optionsBuilder.UseSqlite("Data source=S1159362.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Models.Vak>().HasData(
+            new Models.Vak()
+            {
+                Naam = "server",
+                EC = 4
+            });
+        new Models.Vak()
+        {
+            Naam = "C#",
+            EC = 4
+        };
+        new Models.Vak()
+        {
+            Naam = "Databases",
+            EC = 3
+        };
+        new Models.Vak()
+        {
+            Naam = "UML",
+            EC = 3
+        };
+        new Models.Vak()
+        {
+            Naam = "KBS",
+            EC = 9
+        };
+    }
+}
---
+++ Cijferregistratie/Models/Vak.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cijferregistratie.Models;
+
+public class Vak
+{
+    [Required]
+    public string Naam { get; set; }
+    
+    [Required, MinLength(6)]
+    public int EC { get; set; }
+
+}
